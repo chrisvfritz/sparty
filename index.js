3c8c024d28ce3dc27b22fbcb462f70f1b77e8b0e
Chris Fritz <chrisvfritz@gmail.com>
2017-02-14 12:18:57 -0500

    restrict images to medium thumbnails

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var images = [
-  'https://i.imgur.com/2pxYTLw.jpg',
-  'https://i.imgur.com/WAd25ib.jpg',
-  'https://i.imgur.com/DbiOfQW.jpg',
-  'https://i.imgur.com/VbT7OVM.jpg',
-  'https://i.imgur.com/DxM4nBg.jpg',
-  'https://i.imgur.com/R4piqfs.jpg'
+  'https://i.imgur.com/2pxYTLwm.jpg',
+  'https://i.imgur.com/WAd25ibm.jpg',
+  'https://i.imgur.com/DbiOfQWm.jpg',
+  'https://i.imgur.com/VbT7OVMm.jpg',
+  'https://i.imgur.com/DxM4nBgm.jpg',
+  'https://i.imgur.com/R4piqfsm.jpg'
 ]
 
 module.exports = function () {
